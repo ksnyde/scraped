dcaa160e4aa9881e10798eb8ffe7125af4037bd3
Ken Snyder <ken@ken.net>
2022-02-25 11:22:19 -0800

    chore: remove flattened structures as this is no longer a concern of scrape crate

--- lib/src/util.rs
+++ lib/src/util.rs
@@ -7,3 +7,14 @@ where
 {
     url.to_string().serialize(serializer)
 }
+
+pub fn url_list_to_string<S>(url_list: &Vec<Url>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    url_list
+        .iter()
+        .map(|v| v.to_string())
+        .collect::<Vec<String>>()
+        .serialize(serializer)
+}
